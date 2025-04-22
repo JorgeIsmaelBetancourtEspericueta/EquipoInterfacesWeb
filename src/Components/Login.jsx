41948bcf60f6b7a90e72ec87682b726f3f84ecec
JesusBTE <jeisbetancourtes@ittepic.edu.mx>
2025-04-22 16:42:26 -0700

    Agregando login al proyecto

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,8 @@
 import { useEffect } from "react";
-import { useForm } from "react-hook-form"; //Importamos esto para hacer algunas validaciones
 import "../Login.css";
-import { registerRequest } from "../api/auth.js";
+import { FaUser, FaEnvelope, FaLock } from "react-icons/fa";
 
 export default function Login() {
-  const { register, handleSubmit } = useForm();
-
   useEffect(() => {
     const container = document.getElementById("container");
     const registerBtn = document.getElementById("register");
@@ -19,12 +16,10 @@ export default function Login() {
       container.classList.remove("active");
     });
 
-    // Limpieza de eventos al desmontar el componente
     return () => {
       registerBtn.removeEventListener("click", () => {
         container.classList.add("active");
       });
-
       loginBtn.removeEventListener("click", () => {
         container.classList.remove("active");
       });
@@ -37,37 +32,34 @@ export default function Login() {
         <div className="form-container sign-in">
           <form>
             <h1>Iniciar Sesión</h1>
-            <input type="email" placeholder="Correo Electrónico" />
-            <input type="password" placeholder="Contraseña" />
+            <div className="input-container">
+              <FaEnvelope className="icon" />
+              <input type="email" placeholder="Correo Electrónico" />
+            </div>
+            <div className="input-container">
+              <FaLock className="icon" />
+              <input type="password" placeholder="Contraseña" />
+            </div>
             <a href="#">¿Olvidaste tu contraseña?</a>
             <button type="submit">Ingresar</button>
           </form>
         </div>
 
         <div className="form-container sign-up">
-          <form
-            onSubmit={handleSubmit(async (values) => {
-              console.log(values);
-              const res = await registerRequest(values);
-              console.log(res);
-            })}
-          >
+          <form>
             <h1>Crear Cuenta</h1>
-            <input
-              type="text"
-              {...register("username", { required: true })}
-              placeholder="Nombre"
-            />
-            <input
-              type="email"
-              {...register("email", { required: true })}
-              placeholder="Correo Electrónico"
-            />
-            <input
-              type="password"
-              {...register("password", { required: true })}
-              placeholder="Contraseña"
-            />
+            <div className="input-container">
+              <FaUser className="icon" />
+              <input type="text" placeholder="Nombre" />
+            </div>
+            <div className="input-container">
+              <FaEnvelope className="icon" />
+              <input type="email" placeholder="Correo Electrónico" />
+            </div>
+            <div className="input-container">
+              <FaLock className="icon" />
+              <input type="password" placeholder="Contraseña" />
+            </div>
             <button type="submit">Registrarse</button>
           </form>
         </div>
@@ -77,16 +69,12 @@ export default function Login() {
             <div className="toggle-panel toggle-left">
               <h1>¡Bienvenido de nuevo!</h1>
               <p>Si ya tienes una cuenta, inicia sesión aquí.</p>
-              <button className="hidden" id="login">
-                Iniciar Sesión
-              </button>
+              <button className="hidden" id="login">Iniciar Sesión</button>
             </div>
             <div className="toggle-panel toggle-right">
               <h1>¡Hola, bienvenid@!</h1>
               <p>Regístrate para acceder a todas las funciones del sitio.</p>
-              <button className="hidden" id="register">
-                Registrarse
-              </button>
+              <button className="hidden" id="register">Registrarse</button>
             </div>
           </div>
         </div>
